2a8c6c2216bcaf19c40d2c2d7d2bbbb3462c246b
M4tz01 <matiasborja120@gmail.com>
2026-05-26 23:49:21 -0500

    Util

--- src/util/Validador.java
+++ src/util/Validador.java
@@ -1,4 +1,16 @@
 package util;
 
 public class Validador {
+
+    public static boolean textoVacio(String texto) {
+        return texto.trim().isEmpty();
+    }
+
+    public static boolean validarCorreo(String correo) {
+        return correo.contains("@") && correo.contains(".");
+    }
+
+    public static boolean validarTelefono(String telefono) {
+        return telefono.matches("\\d+");
+    }
 }
